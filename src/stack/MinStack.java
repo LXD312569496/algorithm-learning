d6cd22bd91548747e0ae5541d03dfda5ad2ff097
xiaodong <312569496@qq.com>
2019-03-16 21:09:17 +0800

    栈和数组相关的题目练习

---
+++ src/stack/MinStack.java
@@ -0,0 +1,65 @@
+package stack;
+
+import java.util.Stack;
+
+/**
+ * 155 最小栈
+ * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+ *
+ * push(x) -- 将元素 x 推入栈中。
+ * pop() -- 删除栈顶的元素。
+ * top() -- 获取栈顶元素。
+ * getMin() -- 检索栈中的最小元素。
+ * 示例:
+ *
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ */
+
+
+/**
+ * 思路：利用一个栈，每次插入两个数字：数值+最小值
+ */
+class MinStack {
+
+    private Stack<Integer> stack;
+
+    public MinStack() {
+        stack=new Stack<>();
+    }
+
+    public void push(int x) {
+        if(stack.isEmpty()){
+            stack.push(x);
+            stack.push(x);
+        }else{
+            int tmp = stack.peek();
+            stack.push(x);
+            if(tmp<x){
+                stack.push(tmp);
+            }else{
+                stack.push(x);
+            }
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.get(stack.size()-2);
+    }
+
+    public int getMin() {
+        return stack.peek();
+    }
+}
+
